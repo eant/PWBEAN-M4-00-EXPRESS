272d641cf1bad090be5284bd1b1f1d5b4687452b
smessina <silvio.messina@eant.tech>
2019-09-17 20:09:36 -0300

    Primer version de Web Server con Express

--- app.js
+++ app.js
@@ -1,1 +1,14 @@
 /* Servidor Web hecho con Express y garra */
+const express = require("express")
+
+const server = express()
+
+const port = process.env.PORT || 2000
+
+server.listen( port )
+
+server.get("/", function(request, response){
+
+	response.end("Hola desde Express :D")
+
+})
