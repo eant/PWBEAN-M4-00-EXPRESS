c62d12f00a8045c589638a0122a05f9b0b279ef8
smessina <silvio.messina@eant.tech>
2019-09-17 21:54:31 -0300

    Integrando middleware body-parser

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 /* Servidor Web hecho con Express y garra */
 const express = require("express")
+const bodyParser = require("body-parser")
 
 const server = express()
 
@@ -7,8 +8,29 @@ const port = process.env.PORT || 2000
 
 server.listen( port )
 
-server.get("/", function(request, response){
+server.use( bodyParser.urlencoded({ extended : false }) )
+server.use( bodyParser.json() )
 
-	response.end("Hola desde Express :D")
+server.get("/", function(req, res){
 
+	//res.end("Hola desde Express :D")
+
+	res.sendFile(__dirname + "/index.html")
+
+})
+
+server.get("/noticias", function(req, res){
+	const noticias = [
+		{ titulo : "Aprendiendo Express", autor : "SM", detalle : "En desarrollo..." },
+		{ titulo : "Olvidando Simple Node.js", autor : "SM", detalle : "Hasta la vista, Node" }
+	]
+
+	res.json( noticias )
+})
+
+server.post("/enviar", function(req, res){
+	
+	console.log( req.body.correo )
+
+	res.json( req.body )
 })
